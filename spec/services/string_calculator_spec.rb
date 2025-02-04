8320f50683e7dd5039cd756832c172874dad0761
Akhtar <shoebimran005@gmail.com>
2025-02-04 12:45:27 +0530

    adding an unknown amount of numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -12,5 +12,13 @@ RSpec.describe StringCalculator do
     it 'returns the number itself when one number is given' do
       expect(StringCalculator.add('1')).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it 'returns the sum of multiple numbers' do
+      expect(StringCalculator.add('1,2,3,4')).to eq(10)
+    end
   end
 end
