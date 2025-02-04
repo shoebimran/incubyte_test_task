39b59c6c3f69ce7fdaee96f4e4c421903f3a0d5d
Akhtar <shoebimran005@gmail.com>
2025-02-04 12:40:40 +0530

    Handle returning 0 with empty string &  single number input

---
+++ app/services/string_calculator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# This Services discribe the String calculation function
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+    numbers.to_i
+  end
+end
---
+++ spec/services/string_calculator_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative '../../app/services/string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+
+    it 'returns the number itself when one number is given' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+  end
+end
