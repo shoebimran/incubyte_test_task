de3cd43c8c11377c688025760d9f592535247d4a
Akhtar <shoebimran005@gmail.com>
2025-02-04 13:07:18 +0530

    Allow multiple delimiters, handle multiple delimiters with length longer than one char

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# This Services describe the String calculation function
+# This Service describes the String calculation function
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
@@ -10,8 +10,15 @@ class StringCalculator
     # Check for custom delimiter
     if numbers.start_with?('//')
       parts = numbers.split("\n", 2)
-      delimiter = Regexp.escape(parts[0][2])
+      delimiter_part = parts[0]
       numbers = parts[1]
+
+      if delimiter_part.include?('[')
+        delimiters = delimiter_part.scan(/\[(.*?)\]/).flatten
+        delimiter = delimiters.map { |d| Regexp.escape(d) }.join('|')
+      else
+        delimiter = Regexp.escape(delimiter_part[2])
+      end
     end
 
     nums = numbers.split(/#{delimiter}/).map(&:to_i)
@@ -20,6 +27,6 @@ class StringCalculator
     negatives = nums.select(&:negative?)
     raise "negatives not allowed: #{negatives.join(',')}" unless negatives.empty?
 
-    nums.sum
+    nums.reject { |n| n > 1000 }.sum
   end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -28,5 +28,17 @@ RSpec.describe StringCalculator do
     it 'raises an error for negative numbers' do
       expect { StringCalculator.add('1,-2,3,-4') }.to raise_error('negatives not allowed: -2,-4')
     end
+
+    it 'ignores numbers greater than 1000' do
+      expect(StringCalculator.add('2,1001')).to eq(2)
+    end
+
+    it 'supports multi-character delimiters' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'supports multiple delimiters' do
+      expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
   end
 end
